c17f08905d33f3b4796acdd094fc0167fa95ed22
root <root@Cambridge.localdomain>
2026-05-27 18:12:47 +0000

    add french translations for elapsed time strings

--- test.js
+++ test.js
@@ -85,4 +85,53 @@ console.log(localizedElapsedTimeWithImplicitNow.months.text);
 
 console.log(localizedElapsedTimeWithImplicitNow.years.text);
 
-console.log(localizedElapsedTimeWithImplicitNow.optimal);
+var french = {
+  milliSeconds: ['milliseconde', 's'],
+  seconds: ['seconde', 's'],
+	minutes: ['minute', 's'],
+	hours: ['heure', 's'],
+	days: ['jour', 's'],
+	weeks: ['semaine', 's'],
+	months: ['mois', ''],
+	years: ['an', 's']
+};
+
+var localizedElapsedTimeWithFrench = new Elapsed(then, now, french);
+
+console.log(localizedElapsedTimeWithFrench.milliSeconds.text);
+
+console.log(localizedElapsedTimeWithFrench.seconds.text);
+
+console.log(localizedElapsedTimeWithFrench.minutes.text);
+
+console.log(localizedElapsedTimeWithFrench.hours.text);
+
+console.log(localizedElapsedTimeWithFrench.days.text);
+
+console.log(localizedElapsedTimeWithFrench.weeks.text);
+
+console.log(localizedElapsedTimeWithFrench.months.text);
+
+console.log(localizedElapsedTimeWithFrench.years.text);
+
+console.log(localizedElapsedTimeWithFrench.optimal);
+
+var localizedElapsedTimeWithFrenchImplicitNow = new Elapsed(then, french);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.milliSeconds.text);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.seconds.text);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.minutes.text);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.hours.text);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.days.text);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.weeks.text);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.months.text);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.years.text);
+
+console.log(localizedElapsedTimeWithFrenchImplicitNow.optimal);
