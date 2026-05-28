8fd020c4a05036e55eff5aae98c4b411f8239db1
root <root@Cambridge.localdomain>
2026-05-28 10:12:42 +0000

    add french translations for delay strings

---
+++ fr.d.ts
@@ -0,0 +1,12 @@
+declare const fr: {
+    milliSeconds: [string, string];
+    seconds: [string, string];
+    minutes: [string, string];
+    hours: [string, string];
+    days: [string, string];
+    weeks: [string, string];
+    months: [string, string];
+    years: [string, string];
+};
+
+export = fr;
---
+++ fr.js
@@ -0,0 +1,10 @@
+module.exports = {
+  milliSeconds: ['milliseconde', 's']
+  , seconds: ['seconde', 's']
+	, minutes: ['minute', 's']
+	, hours: ['heure', 's']
+	, days: ['jour', 's']
+	, weeks: ['semaine', 's']
+	, months: ['mois', '']
+	, years: ['année', 's']
+};
