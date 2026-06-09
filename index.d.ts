4b4af20c318ad2c73877c8044abbb435e7be1e74
root <root@Cambridge.localdomain>
2026-06-09 16:35:10 +0000

    Add French translations for delay strings

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,25 @@
+declare const l10nFrench: {
+  milliSeconds: [string, string];
+  seconds: [string, string];
+  minutes: [string, string];
+  hours: [string, string];
+  days: [string, string];
+  weeks: [string, string];
+  months: [string, string];
+  years: [string, string];
+};
+
 export = Elapsed;
 declare function Elapsed(from: any, to: any, l10n: any, ...args: any[]): void;
-declare class Elapsed {
-    constructor(from: any, to: any, l10n: any, ...args: any[]);
-    from: any;
-    to: any;
-    l10n: any;
-    set(): Elapsed;
-    elapsedTime: number;
-    milliSeconds: {
-        num: number;
-    };
-    seconds: {
-        num: number;
-    };
-    minutes: {
-        num: number;
-    };
-    hours: {
-        num: number;
-    };
-    days: {
-        num: number;
-    };
-    weeks: {
-        num: number;
-    };
-    months: {
-        num: number;
-    };
-    years: {
-        num: number;
-    };
-    optimal: any;
-    refresh(to: any): Elapsed;
+declare namespace Elapsed {
+  const l10nFrench: {
+    milliSeconds: [string, string];
+    seconds: [string, string];
+    minutes: [string, string];
+    hours: [string, string];
+    days: [string, string];
+    weeks: [string, string];
+    months: [string, string];
+    years: [string, string];
+  };
 }
